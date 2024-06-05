dd507071eae0cc163b09bc5de2de9adb09da5e4f
Afolabi Esther <starafolab@gmail.com>
2024-06-05 17:33:03 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+An Expense Splitter API
---
+++ app/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const rotatingFileStream = require('../config/logger')
+require('dotenv').config();
+
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+const app = express();
+
+app.use(logger('combined', { stream: rotatingFileStream }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
